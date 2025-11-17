6b9cc22109e22e9167b4c31ad083594d1bc52a6f
Jhonatan Rocha <118951894+JhonatanSr18@users.noreply.github.com>
2025-11-17 16:55:11 -0300

    Use environment variables for email config

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# -----------------------------
-# CONFIGURAÇÃO DE EMAIL (GMAIL)
-# -----------------------------
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = ('Last Island Support', 'egobebygu2@gmail.com')
+import os
+
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
+app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT"))
+app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == "True"
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_DEFAULT_SENDER")
+
 
 mail = Mail(app)
 
